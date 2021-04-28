4d201bf17bedbca9d2ede957614fd921a34284f4
J. Nunn <jbnunn@gmail.com>
2021-04-28 10:26:31 -0500

    Imported from Alexa Cookbook

---
+++ resources.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2020 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+ *
+ * Licensed under the Amazon Software License (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/asl/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ **/
+'use strict';
+
+module.exports = {
+    en: {
+        translation: {
+            WELCOME: `Welcome to the Pizza Reference. Please tell me your name to begin.`,
+            REMOVED_NAME: `Okay, I've removed your name: please provide your name again, if you want to start a pizza order.`,
+            NO_NAME_PROMPT: `I currently don't know your name. Please provide your name to start.`,
+            HELP_INTENT: `To begin your a pizza order please provide your name by saying "call me Dennis", or "Dennis".`,
+            ERROR: `Sorry, something went wrong. Please try again later.`,
+            GENERIC_REPROMPT: `Sorry, I didn't catch that, would you like to order a pizza or maybe hear our specials?`,
+            EXIT: `Goodbye!`,
+        }
+    }
+};
